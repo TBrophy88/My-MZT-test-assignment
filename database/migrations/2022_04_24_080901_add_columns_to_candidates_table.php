d9e33400749d9aa32350d1608ea67089ff841d73
Tom <t.brophy@gmail.com>
2022-04-24 16:18:12 +0800

    database migrations

---
+++ database/migrations/2022_04_24_080901_add_columns_to_candidates_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('candidates', function (Blueprint $table) {
+            $table->boolean('contacted')->default(FALSE);
+            $table->boolean('hired')->default(FALSE);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('candidates', function (Blueprint $table) {
+            $table->dropColumn(['contacted', 'hired']);
+        });
+    }
+};
